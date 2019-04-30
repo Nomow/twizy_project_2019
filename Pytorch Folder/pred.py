183b75f89df413192796bfc76d721904ebca3c3c
AlexandrePerdereau <43601699+AlexandrePerdereau@users.noreply.github.com>
2019-04-30 11:51:23 +0200

    1semaine

--- Pytorch Folder/pred.py
+++ Pytorch Folder/pred.py
@@ -4,7 +4,7 @@ Created on Mon Apr 29 18:34:38 2019
 
 @author: guill
 """
-
+print("testpy0")
 import time
 import cv2
 
@@ -16,10 +16,20 @@ from torchvision import transforms
 from PIL import Image
 import fastai
 
+import argparse
+print("testpy")
+ 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+help='an integer for the accumulator')
+ 
+args = parser.parse_args()
 
-defaults.device = torch.device('cpu')
-img = open_image(r"C:\Users\guill\Downloads\dataDL\inferences2\test6.jpg")
-learn = load_learner(path =r"C:\Users\guill\Downloads\dataDL")
+
+
+defaults.device = torch.device('cuda')
+img = open_image(r"C:\Users\alexa\Desktop\twizy_project_2019\temporaryFiles\tempImTe"+args+".png")
+learn = load_learner(path =r"C:\Users\alexa\Desktop\twizy_project_2019\Pytorch Folder")
 pred_class,pred_idx,outputs = learn.predict(img)
 print(pred_class)
 
--- test.py
+++ test.py
@@ -6,3 +6,39 @@ Created on Mon Apr 29 04:07:04 2019
 """
 
 print ("Hello World from python")
+print("mmmmm")
+
+import time
+import cv2
+print("mmm")
+import numpy
+from fastai.vision import *
+from fastai import *
+from pathlib import Path
+from torchvision import transforms
+from PIL import Image
+import fastai
+print("mmmmm")
+import argparse
+print("mmmmm")
+# 
+#parser = argparse.ArgumentParser(description='Process some integers.')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+',
+#help='an integer for the accumulator')
+# 
+#args = parser.parse_args()
+#i=args.accumulate(args.integers)
+#print("mhhhhhh"+i)
+#
+#
+#
+#defaults.device = torch.device('cuda')
+#img = open_image(r"C:\Users\alexa\Desktop\twizy_project_2019\TemporaryFiles\tempIm"+i+".png")
+#learn = load_learner(path =r"C:\Users\alexa\Desktop\twizy_project_2019\Pytorch Folder")
+#pred_class,pred_idx,outputs = learn.predict(img)
+#print ("Hello World from python!")
+#print("testpy")
+#print(pred_class)
+#fichier = open("C:\Users\alexa\Desktop\twizy_project_2019\TemporaryFiles\result"+i+".txt", "w")
+#fichier.write(""+pred_class)
+#fichier.close()
